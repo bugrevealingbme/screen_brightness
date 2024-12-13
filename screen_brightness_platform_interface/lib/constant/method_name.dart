bc3c44794a25e4e5f13dcbd2107dd63379bff1eb
Burak Mete Erdoğan <bugrevealingbme@gmail.com>
2024-12-13 22:45:33 +0300

    Update method_name.dart

--- screen_brightness_platform_interface/lib/constant/method_name.dart
+++ screen_brightness_platform_interface/lib/constant/method_name.dart
@@ -1,6 +1,5 @@
 const methodNameGetSystemScreenBrightness = 'getSystemScreenBrightness';
 const methodNameSetSystemScreenBrightness = 'setSystemScreenBrightness';
-const methodNameCheckSystemCanWrite = 'checkSystemCanWrite';
 const methodNameGetApplicationScreenBrightness =
     'getApplicationScreenBrightness';
 const methodNameSetApplicationScreenBrightness =
